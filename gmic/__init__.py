4b96784f4d109d514412867d7cda9dad417d1d6a
cedspam <cedric.lacrambe@gmail.com>
2019-03-29 13:17:01 +0100

    Update __init__.py

--- gmic/__init__.py
+++ gmic/__init__.py
@@ -1,6 +1,8 @@
+import os
 from gmicpy  import  gmic_call as gmic
 from gmicpy  import  reset
 from gmicpy  import  _init_gmic_instance
+from gmicpy  import add_commands,path_rc,gmic_version
 _init_gmic_instance()
 del _init_gmic_instance
 name = "gmic"
