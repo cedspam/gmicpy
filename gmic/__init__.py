46b7a92f08095be8bea5a255ae6e21f6c72b127a
cedspam <LT24927@eu.labinal.snecma>
2019-04-02 15:52:49 +0200

    load gmic scrip archive

--- gmic/__init__.py
+++ gmic/__init__.py
@@ -1,4 +1,7 @@
 import os
+import glob
+import inspect
+import tarfile
 from gmicpy  import  gmic_call as gmic
 from gmicpy  import  reset
 from gmicpy  import  _init_gmic_instance
@@ -6,6 +9,19 @@ from gmicpy  import add_commands,path_rc,gmic_version
 _init_gmic_instance()
 del _init_gmic_instance
 #"%supdate%u.gmic"
+modulepath=os.path.abspath(inspect.getfile(inspect.currentframe()))
+moduledir=os.path.join(os.path.dirname(modulepath))
+with tarfile.open(os.path.join(moduledir,"include.tar.bz2"), "r") as tar:
+    for tarinfo in tar:
+        if tarinfo.name.endwith(".gmic"):
+            print("parsing:",tarinfo.name)
+            with tar.extractfile(tarinfo) as f:
+                add_commands(f.read)
+
+
+del tarinfo
+del tar
+
 
 
 name = "gmic"
